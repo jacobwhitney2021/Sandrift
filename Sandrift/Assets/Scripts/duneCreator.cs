4ff3cc849596b1c63a47453c812b65f92ac7569a
bayley105 <79952159+bayley105@users.noreply.github.com>
2021-03-28 22:34:22 -0400

    Dune Generator?

---
+++ Sandrift/Assets/Scripts/duneCreator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class duneCreator : MonoBehaviour
+{
+    public float height;
+    public float thickness;
+    //public float length;
+    //public float shape ??? ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //instantiate ----
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ Sandrift/Assets/Scripts/testForce.cs
@@ -0,0 +1,26 @@
+﻿// The sprite will fall under its weight.  After a short time the
+// sprite will start its upwards travel due to the thrust force that
+// is added in the opposite direction.
+
+using UnityEngine;
+using System.Collections;
+
+public class testForce : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    public float thrust = 3f;
+    public Vector2 steepnessVector;
+
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        steepnessVector = gameObject.GetComponent<steepnessMap>().steepnessVectorAtPoint;
+        rb2D.AddForce(steepnessVector * thrust);
+    }
+
+
+}
