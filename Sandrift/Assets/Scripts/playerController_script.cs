bb4953a8a33ba27ee61e881a2f8d61413ec81fda
bayley105 <79952159+bayley105@users.noreply.github.com>
2021-04-01 16:02:31 -0400

    created player controls for sail

---
+++ Sandrift/Assets/Scripts/playerController_script.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController_script : MonoBehaviour
+{
+	// Preliminary controls proposal:
+
+	// the way im thinking of mapping controls is that WASD is non-wind/rocket controls and UpDownLeftRight keys are for the sail controls
+	// so A and D will control the rotation of the craft and W and S will increase/decrease rocket thrust
+	// left and right will rotate the sail and up and down will raise/lower the sail    
+
+	// whoever works on this next, feel free to change any of this to make it feel better
+
+	public float sailAngle = 0.0f;
+	public float sailFullness = 0.0f; 
+
+	public float sailPivotSpeed = 10.0f; // speed of rotation of the sail
+	public float sailFullnessSpeed = 1.5f; // speed that the sail changed fullness
+
+	public float fullness_change;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float rotation_change = Input.GetAxis("SailHorizontal") * sailPivotSpeed * Time.deltaTime;
+        sailAngle += rotation_change;
+        if (sailAngle<0f) {
+        	sailAngle += 360f;
+        }
+        if (sailAngle>360f) {
+        	sailAngle -= 360f;
+        }
+
+
+        fullness_change = Input.GetAxis("SailVertical") * sailFullnessSpeed * Time.deltaTime;
+        if (sailFullness+fullness_change<0) {
+        	sailFullness = 0f;
+        }
+        else if (sailFullness+fullness_change>1) {
+        	sailFullness = 1f;
+        }
+        else {
+        	sailFullness += fullness_change;
+        }
+
+
+        
+
+    }
+
+
+}
--- Sandrift/Assets/Scripts/windForce_script.cs
+++ Sandrift/Assets/Scripts/windForce_script.cs
@@ -6,6 +6,7 @@ public class windForce_script : MonoBehaviour
 {
     public Vector3 windVector;
     public float sailAngle;
+    public float sailFullness;
 
     private Rigidbody rigidbody;
 
@@ -19,19 +20,21 @@ public class windForce_script : MonoBehaviour
 
     void Update()
     {
-        sailForce = calculateSailForce(windVector, rigidbody.velocity, sailAngle);
+        sailAngle = gameObject.GetComponent<playerController_script>().sailAngle;
+        sailFullness = gameObject.GetComponent<playerController_script>().sailFullness;
+        sailForce = calculateSailForce(windVector, rigidbody.velocity, sailAngle, sailFullness); // do i have to do time delta time for this????? or leave it 
         rigidbody.AddForce(sailForce);
     }
 
-    Vector3 calculateSailForce(Vector3 windVector, Vector3 playerVelocity, float sailAngle)
+    Vector3 calculateSailForce(Vector3 windVector, Vector3 playerVelocity, float sailAngle, float sailFullness)
     {
     	Vector3 apparentWindVector = windVector - playerVelocity;
 		float windAngle = Vector3.Angle(new Vector3(0,0,1), apparentWindVector); // angle from global forward. need to make sure sailAngle is defined in a similar way (i.e polar coordinates)
 		float sailWindAngle = Mathf.Abs(sailAngle - windAngle);
 		// sailforcevecotr hsould not be based of wind vector but should be sailvector modified by angle between windvector and sail angle, further modified by difference between windspeed and craft speed (zero force if speeds are the same)
     	Vector3 sailForceVector;
-    	if ((sailWindAngle <= Mathf.PI) || (sailWindAngle >= (3/4)*Mathf.PI)) { // this is not working, seems to always be true
-    		sailForceVector = transform.forward * apparentWindVector.magnitude * Mathf.Cos(sailWindAngle); // maybe make square of wind speed
+    	if ((sailWindAngle <= 90f) || (sailWindAngle >= 270f)) { // this is not working, seems to always be true   || (sailWindAngle >= (3/2)*Mathf.PI)
+    		sailForceVector = transform.forward * apparentWindVector.magnitude * Mathf.Cos(sailWindAngle*(Mathf.PI/180f)) * sailFullness; // maybe make square of wind speed
     	}
     	else {
     		sailForceVector = Vector3.zero;
