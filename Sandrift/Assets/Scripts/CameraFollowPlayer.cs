8e637bc624a2308e1500934611f53ff260f9b1af
jacobwhitney2021 <80171323+jacobwhitney2021@users.noreply.github.com>
2021-04-05 20:34:16 -0400

    CameraFollowsPlayer

--- Sandrift/Assets/Scripts/CameraFollowPlayer.cs
+++ Sandrift/Assets/Scripts/CameraFollowPlayer.cs
@@ -1,17 +1,17 @@
 ﻿using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class CameraFollowPlayer : MonoBehaviour
 {
     public Transform target;
 
     public float smoothSpeed = 0.45f;
     public Vector3 offset;
 
-    void LateUpdate ()
+    void LateUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
     }
-    
+
 }
