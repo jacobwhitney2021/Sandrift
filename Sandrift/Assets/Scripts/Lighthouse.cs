97833c8be2c54d11bdb2bc622ba766e83fe29e8a
mayagangan <80071869+mayagangan@users.noreply.github.com>
2021-04-20 15:31:20 -0400

    Added Collision Trigger

--- Sandrift/Assets/Scripts/Lighthouse.cs
+++ Sandrift/Assets/Scripts/Lighthouse.cs
@@ -5,24 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Lighthouse : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    void OnCollisionEnter(Collision gameObjectInformation)
-    {
-        if (gameObjectInformation.gameObject.name== "PlayerPrefab")
-        {
-            Debug.Log("Collision Detected");
-            SceneManager.LoadScene("EndMenu");
-        }
-        
+        SceneManager.LoadScene("EndMenu");
     }
 }
