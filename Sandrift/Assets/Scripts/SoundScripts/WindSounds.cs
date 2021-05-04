a6b3813bdd99d15252c97b9f2f168e44143c08c8
jacobwhitney2021 <80171323+jacobwhitney2021@users.noreply.github.com>
2021-05-04 17:27:41 -0400

    organizing sound scripts, start of wind sounds, adding variation to wind sound in game

---
+++ Sandrift/Assets/Scripts/SoundScripts/WindSounds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindSounds : MonoBehaviour
+{
+
+    public AudioSource HeavyWindSound;
+    public GameObject sail;
+
+    private windForce_script windForces;
+
+    public float windMag;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        sail = GameObject.Find("sail");
+        windForces = sail.GetComponent<windForce_script>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        windMag = windForces.magnitude;
+        Debug.Log(windMag);
+    }
+}
