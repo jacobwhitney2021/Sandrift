9ef790bb23c4f286981cf14ec3dae3df042a8faf
jacobwhitney2021 <80171323+jacobwhitney2021@users.noreply.github.com>
2021-05-04 16:52:39 -0400

    end menu and wind sound on main menu

---
+++ Sandrift/Assets/Scripts/VolumePingPong.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumePingPong : MonoBehaviour
+{
+
+    public AudioSource MenuWind;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        MenuWind.volume = Mathf.PingPong(Time.time/6, .5f) + .5f;
+        MenuWind.pitch = Mathf.PingPong(Time.time / 10, .25f) + .75f;
+    }
+}
