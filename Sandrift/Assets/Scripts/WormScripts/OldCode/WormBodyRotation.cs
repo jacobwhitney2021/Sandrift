ef4f5f551a7d152a3647e2c213a15817f870887b
jclark10 <james.clark@tufts.edu>
2021-05-11 00:43:17 -0400

    Made WormV2

---
+++ Sandrift/Assets/Scripts/WormScripts/OldCode/WormBodyRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormBodyRotation : MonoBehaviour
+{
+    public float rotSpeed;
+
+    private Vector3 direction;
+
+    public Transform target;
+
+    void Update()
+    {
+        direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg - 180f;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
+        float step = rotSpeed * Time.deltaTime;
+
+        transform.rotation = Quaternion.Slerp(
+                                transform.rotation,
+                                rotation,
+                                step);
+    }
+}
